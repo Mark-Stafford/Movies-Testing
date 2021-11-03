55f71986442e1719862fd0742f34a743fe2af059
“Mark <markstaff2@gmail.com>
2021-11-03 11:53:32 +0000

    E2E tests - Movie Details page - poster images.

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -6,89 +6,108 @@ const filterByTitle = (movieList, string) =>
 
 const filterByGenre = (movieList, genreId) =>
   movieList.filter((m) => m.genre_ids.includes(genreId));
-  describe("Home Page ", () => {
-    before(() => {
-      // Get movies from TMDB and store in movies variable.
-      cy.request(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
-          "TMDB_KEY"
-        )}&language=en-US&include_adult=false&include_video=false&page=1`
-      )
-        .its("body")    // Take the body of HTTP response from TMDB
-        .then((response) => {
-          movies = response.results
-        })
-    })
-    beforeEach(() => {
-      cy.visit("/")
-    });
 
-    describe("Home Page ", () => {
-      before(() => { 
-        cy.visit("/")
-      });
-  
-    describe("Base test", () => {
-      it("displays page header", () => {
+const filterByGenreAndTitle = (movieList, genreId, string) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId) && m.title.toLowerCase().search(string) !== -1);
+
+describe("Home Page ", () => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")    // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results
+      })
+  })
+  beforeEach(() => {
+    cy.visit("/")
+  });
+
+  describe("Base tests", () => {
+    it("displays page header", () => {
         cy.get("h3").contains("Discover Movies");
         cy.get("h1").contains("Filter the movies");
       });
-    });
-  })
+  });
 
   describe("Filtering", () => {
     describe("By movie title", () => {
-     it("should only display movies with m in the title", () => {
-       let searchString = "m";
-       let matchingMovies = filterByTitle(movies, searchString);
-       cy.get("#filled-search").clear().type(searchString); // Enter m in text box
-       cy.get(".MuiCardHeader-content").should(
-         "have.length",
-         matchingMovies.length
-       );
-       cy.get(".MuiCardHeader-content").each(($card, index) => {
-         cy.wrap($card).find("p").contains(matchingMovies[index].title);
-       });
-     })
-     it("should only display movies with o in the title", () => {
-       let searchString = "o";
-       let matchingMovies = filterByTitle(movies, searchString);
-       cy.get("#filled-search").clear().type(searchString); // Enter m in text box
-       cy.get(".MuiCardHeader-content").should(
-         "have.length",
-         matchingMovies.length
-       );
-       cy.get(".MuiCardHeader-content").each(($card, index) => {
-         cy.wrap($card).find("p").contains(matchingMovies[index].title);
-       });
-      })
-       it("should display no movies when with xyz is typed", () => {
+        it("should only display movies with m in the title", () => {
+          let searchString = "m";
+          let matchingMovies = filterByTitle(movies, searchString);
+          cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+          cy.get(".MuiCardHeader-content").should(
+            "have.length",
+            matchingMovies.length
+          );
+          cy.get(".MuiCardHeader-content").each(($card, index) => {
+            cy.wrap($card).find("p").contains(matchingMovies[index].title);
+          });
+        })
+        it("should only display movies with o in the title", () => {
+          let searchString = "o";
+          let matchingMovies = filterByTitle(movies, searchString);
+          cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+          cy.get(".MuiCardHeader-content").should(
+            "have.length",
+            matchingMovies.length
+          );
+          cy.get(".MuiCardHeader-content").each(($card, index) => {
+            cy.wrap($card).find("p").contains(matchingMovies[index].title);
+          });
+        });
+      it("should only display movies with xyz in the title", () => {
         let searchString = "xyz";
         let matchingMovies = filterByTitle(movies, searchString);
-        cy.get("#filled-search").clear().type(searchString); // Enter xyz in text box
+        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
         cy.get(".MuiCardHeader-content").should(
           "have.length",
           matchingMovies.length
         );
-      })
-    });
-       
-     describe("By movie genre", () => {
-      it("should display movies with the specified genre only", () => {
-         const selectedGenreId = 35;
-         const selectedGenreText = "Comedy";
-         const matchingMovies = filterByGenre(movies, selectedGenreId);
-         cy.get("#genre-select").click();
-         cy.get("li").contains(selectedGenreText).click();
-         cy.get(".MuiCardHeader-content").should(
-           "have.length",
-           matchingMovies.length
-         );
-         cy.get(".MuiCardHeader-content").each(($card, index) => {
-           cy.wrap($card).find("p").contains(matchingMovies[index].title);
-         });
+      });
+    })
+  });
+
+  describe("By movie genre", () => {
+    it("should display movies with the specified genre only", () => {
+       const selectedGenreId = 35;
+       const selectedGenreText = "Comedy";
+       const matchingMovies = filterByGenre(movies, selectedGenreId);
+       cy.get("#genre-select").click();
+       cy.get("li").contains(selectedGenreText).click();
+       cy.get(".MuiCardHeader-content").should(
+         "have.length",
+         matchingMovies.length
+       );
+       cy.get(".MuiCardHeader-content").each(($card, index) => {
+         cy.wrap($card).find("p").contains(matchingMovies[index].title);
        });
      });
+   });
+
+   describe("By Title and Movie genre", () => {
+    it("should display movies with the specified genre and title only", () => {
+     const searchString = "o";
+     const genreId = 35;
+     const genreText = "Comedy";
+     const matchingMovies = filterByGenreAndTitle(movies, genreId, searchString);
+
+     cy.get("#filled-search").clear().type(searchString);
+     cy.get("#genre-select").click();
+     cy.get("li").contains(genreText).click();
+
+     cy.get(".MuiCardHeader-content").should(
+        "have.length",
+        matchingMovies.length
+      );
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(matchingMovies[index].title);
+      });
+    });
    });
   });
 
---
+++ cypress/integration/movieDetails-page-spec.js
@@ -0,0 +1,43 @@
+let movieId = 335983; // The movie Venom
+let movie;
+let reviews;
+
+describe("Movie Details Page", () => {
+  before(() => {
+    cy.request(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}`
+    )
+      .its("body")
+      .then((movieDetails) => {
+        movie = movieDetails;
+        return movieDetails.id;
+      });
+  });
+  beforeEach(() => {
+    cy.visit(`/movies/${movie.id}`);
+  });
+ 
+    it("should display movie title in the page header", () => {
+        cy.get("h3").contains(movie.title);
+    });
+
+    it("should display the movie's details", () => {
+        cy.get("h3").contains("Overview");
+        cy.get("h3").next().contains(movie.overview);
+        cy.get("ul")
+            .eq(1)
+            .within(() => {
+            const genreChips = movie.genres.map((g) => g.name);
+            genreChips.unshift("Genres");
+            cy.get("span").each(($card, index) => {
+                cy.wrap($card).contains(genreChips[index]);
+            });
+            });
+        });
+
+    it("should display movie posters on the left side", () => {
+        cy.get('img').should('have.css', 'text-align', 'left');
+    });
+});
