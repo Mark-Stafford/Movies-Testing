74703b087f5b5f17e8fdf61a155eaedba0a33d68
“Mark <markstaff2@gmail.com>
2021-12-14 12:07:25 +0000

    fixed error

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+//Todo fix with environment varaibles
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAd9OP6otVdqYxqtq3_EToQQZCR7AkvQos",
+    authDomain: "react-6aceb.firebaseapp.com",
+    projectId: "react-6aceb",
+    storageBucket: "react-6aceb.appspot.com",
+    messagingSenderId: "238610559223",
+    appId: "1:238610559223:web:026c5ef309c5f8bd61a3f2",
+    measurementId: "G-HHJNRDE8VB"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const auth = getAuth(app);
